51f3b202080bb35d18d19fa9a2a2b083dba3c5d6
EDA <eda@mac-mini-3.local>
2016-09-06 15:47:12 +1200

    added readFile and JSON code

--- server.js
+++ server.js
@@ -2,8 +2,9 @@ var express = require('express')
 var app = express()
 
 var hbs = require('express-handlebars')
-var blogs = require('./blogs')
+// var blogs = require('./blogs')
 var path = require('path')
+var fs = require('fs')
 
 app.listen(3020, function () {
   console.log('listening on port 3020')
@@ -14,9 +15,22 @@ app.set('view engine', 'hbs')
 app.set('views', path.join(__dirname, 'views'))
 
 app.get('/', function (req, res) {
-  res.render('index', blogs)
+  // res.render('index', blogs)
+  getBlogs(function (err, blogs) {
+    res.render('index', blogs)
+  })
 })
 
 app.get('/blog/:id', function (req, res) {
-  res.render('blogpost', blogs.blogs[req.params.id])
+  // res.render('blogpost', blogs.blogs[req.params.id])
+  getBlogs(function (err, blogs) {
+    res.render('blogpost', blogs.blogs[req.params.id])
+  })
 })
+
+function getBlogs (cb) {
+  fs.readFile("blog.json", function (err, contents){
+    var blogs = JSON.parse(contents)
+    cb(null, blogs)
+  })
+}
